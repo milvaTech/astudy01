2077c23384420d31c7a5f42d9afffebe2a066497
MilvaTech <webmilva@gmail.com>
2023-11-12 04:56:50 -0800

    Initial Commit. very basic impl of action

---
+++ src/main/java/com/sta/edu/astudy01/Astudy01Application.java
@@ -0,0 +1,20 @@
+package com.sta.edu.astudy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Astudy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Astudy01Application.class, args);
+	}
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to MilvaTech";
+	}
+}
---
+++ src/test/java/com/sta/edu/astudy01/Astudy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sta.edu.astudy01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Astudy01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
